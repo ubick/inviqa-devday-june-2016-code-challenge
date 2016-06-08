9671c6700a4fc3469442a1ab87669c5ee69897b9
Liviu Panainte <lpanainte@inviqa.com>
2016-06-08 08:59:15 +0100

    Add description

--- src/Inviqa/Challenge/Basket.php
+++ src/Inviqa/Challenge/Basket.php
@@ -17,6 +17,18 @@ class Basket
         $this->products[] = $product;
     }
 
+    /*
+     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+     * Inviqa June 2016 Coding Challenge                                                 *
+     *                                                                                   *
+     * 2 for 1 discount: 2 products of the same kind only cost the price of 1            *
+     *                                                                                   *
+     * A product can only be one of:                                                     *
+     *  - Inviqa\Challenge\Product\Book                                                  *
+     *  - Inviqa\Challenge\Product\Cookie                                                *
+     *  - Inviqa\Challenge\Product\Pen                                                   *
+     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+     */
     public function totalPrice()
     {
         // TODO Write your solution here!
