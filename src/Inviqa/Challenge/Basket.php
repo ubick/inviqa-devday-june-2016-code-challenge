e91831961f96100022a876db662c5158d15fb584
Liviu Panainte <lpanainte@inviqa.com>
2016-06-08 08:55:01 +0100

    Add TODO

--- src/Inviqa/Challenge/Basket.php
+++ src/Inviqa/Challenge/Basket.php
@@ -19,25 +19,6 @@ class Basket
 
     public function totalPrice()
     {
-        $total = 0;
-        $books = 0;
-        $pens = 0;
-        $cookies = 0;
-
-        foreach ($this->products as $product) {
-            if ($product instanceof Book) $books++;
-            if ($product instanceof Pen) $pens++;
-            if ($product instanceof Cookie) $cookies++;
-
-            $total += $product->getPrice();
-        }
-
-        $booksDiscount = (int)($books / 2) * Book::PRICE;
-        $pensDiscount = (int)($pens / 2) * Pen::PRICE;
-        $cookiesDiscount = (int)($cookies / 2) * Cookie::PRICE;
-
-        $discount = $booksDiscount + $pensDiscount + $cookiesDiscount;
-
-        return $total - $discount;
+        // TODO Write your solution here!
     }
 }
